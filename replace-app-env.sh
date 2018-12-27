8481501fab5b6c26c091de81c2f79b46504192d5
MaiReo <sawako.saki@gmail.com>
2018-12-27 12:28:24 +0800

    Create replace-app-env.sh

---
+++ replace-app-env.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+BUILD_ENV=$BUILD_ENV
+REMOTE_SERVICE_BASE_URL=$REMOTE_SERVICE_BASE_URL
+APP_BASE_URL=$APP_BASE_URL
+UPLOAD_SERVER_URL=$UPLOAD_SERVER_URL
+rm -rf src/environments/environment*.ts
+rm -rf src/assets/appconfig*.json
+mkdir -p src/environments
+curl -s https://raw.githubusercontent.com/MaiReo/housecool-com-scripts/master/src/environments/environment.ts.template \
+  | sed "s/{APP_CONFIG}/.${BUILD_ENV}/g" \
+  | sed "s/{UPLOAD_SERVER_URL}/${UPLOAD_SERVER_URL}/g" \
+  | tee src/environments/environment.${BUILD_ENV}.ts
+  
+curl -s https://raw.githubusercontent.com/MaiReo/housecool-com-scripts/master/src/assets/appconfig.json.template \
+  | sed "s/{REMOTE_SERVICE_BASE_URL}/${REMOTE_SERVICE_BASE_URL}/g" \
+  | sed "s/{APP_BASE_URL}/${APP_BASE_URL}/g" \
+  | tee src/assets/appconfig.${BUILD_ENV}.json
