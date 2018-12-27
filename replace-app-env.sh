669bf3c69e9ecd2926ed93c89262f89478cebfa9
MaiReo <sawako.saki@gmail.com>
2018-12-27 12:56:23 +0800

    Update replace-app-env.sh

--- replace-app-env.sh
+++ replace-app-env.sh
@@ -1,8 +1,11 @@
-#/bin/bash
+#!/bin/bash
 BUILD_ENV=$BUILD_ENV
-REMOTE_SERVICE_BASE_URL=$REMOTE_SERVICE_BASE_URL
-APP_BASE_URL=$APP_BASE_URL
-UPLOAD_SERVER_URL=$UPLOAD_SERVER_URL
+REMOTE_SERVICE_BASE_URL=${REMOTE_SERVICE_BASE_URL//\:/\\:}
+REMOTE_SERVICE_BASE_URL=${REMOTE_SERVICE_BASE_URL//\//\\/}
+APP_BASE_URL=${APP_BASE_URL//\:/\\:}
+APP_BASE_URL=${APP_BASE_URL//\//\\/}
+UPLOAD_SERVER_URL=${UPLOAD_SERVER_URL//\:/\\:}
+UPLOAD_SERVER_URL=${UPLOAD_SERVER_URL//\//\\/}
 rm -rf src/environments/environment*.ts
 rm -rf src/assets/appconfig*.json
 mkdir -p src/environments
@@ -10,7 +13,7 @@ curl -s https://raw.githubusercontent.com/MaiReo/housecool-com-scripts/master/sr
   | sed "s/{APP_CONFIG}/.${BUILD_ENV}/g" \
   | sed "s/{UPLOAD_SERVER_URL}/${UPLOAD_SERVER_URL}/g" \
   | tee src/environments/environment.${BUILD_ENV}.ts
-  
+
 curl -s https://raw.githubusercontent.com/MaiReo/housecool-com-scripts/master/src/assets/appconfig.json.template \
   | sed "s/{REMOTE_SERVICE_BASE_URL}/${REMOTE_SERVICE_BASE_URL}/g" \
   | sed "s/{APP_BASE_URL}/${APP_BASE_URL}/g" \
