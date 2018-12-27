f418b836ebb69e9f8d922fd0b2187ede97f179ed
MaiReo <sawako.saki@gmail.com>
2018-12-27 13:13:18 +0800

    Update replace-app-env.sh

--- replace-app-env.sh
+++ replace-app-env.sh
@@ -13,7 +13,7 @@ curl -s https://raw.githubusercontent.com/MaiReo/housecool-com-scripts/master/sr
   | sed "s/{APP_CONFIG}/.${BUILD_ENV}/g" \
   | sed "s/{UPLOAD_SERVER_URL}/${UPLOAD_SERVER_URL}/g" \
   | tee src/environments/environment.ts
-
+cp -v src/environments/environment.ts src/environments/environment.prod.ts
 curl -s https://raw.githubusercontent.com/MaiReo/housecool-com-scripts/master/src/assets/appconfig.json.template \
   | sed "s/{REMOTE_SERVICE_BASE_URL}/${REMOTE_SERVICE_BASE_URL}/g" \
   | sed "s/{APP_BASE_URL}/${APP_BASE_URL}/g" \
