b8699ddf5cc64ff8698e5147647a0c562c7249cd
MaiReo <sawako.saki@gmail.com>
2019-01-25 15:05:49 +0800

    Update replace-app-env.sh

--- replace-app-env.sh
+++ replace-app-env.sh
@@ -12,6 +12,7 @@ mkdir -p src/environments
 curl -s https://raw.githubusercontent.com/MaiReo/housecool-com-scripts/master/src/environments/environment.ts.template \
   | sed "s/{APP_CONFIG}/.${BUILD_ENV}/g" \
   | sed "s/{UPLOAD_SERVER_URL}/${UPLOAD_SERVER_URL}/g" \
+  | sed "s/{SERVER_URL}/${REMOTE_SERVICE_BASE_URL}/g" \
   | tee src/environments/environment.ts
 cp -v src/environments/environment.ts src/environments/environment.prod.ts
 curl -s https://raw.githubusercontent.com/MaiReo/housecool-com-scripts/master/src/assets/appconfig.json.template \
