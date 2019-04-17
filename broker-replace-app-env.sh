f26d5953bf8d1fa9251a008a008bd4719c5254ba
MaiReo <sawako.saki@gmail.com>
2019-04-17 18:57:52 +0800

    Create broker-replace-app-env.sh

---
+++ broker-replace-app-env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SERVER_URL=${SERVER_URL//\:/\\:}
+SERVER_URL=${SERVER_URL//\//\\/}
+SIGNALR_SERVER_URL=${SIGNALR_SERVER_URL//\:/\\:}
+SIGNALR_SERVER_URL=${SIGNALR_SERVER_URL//\//\\/}
+UPLOAD_SERVER_URL=${UPLOAD_SERVER_URL//\:/\\:}
+UPLOAD_SERVER_URL=${UPLOAD_SERVER_URL//\//\\/}
+rm -rf src/environments/environment*.ts
+rm -rf src/assets/appconfig*.json
+mkdir -p src/environments
+curl -s https://raw.githubusercontent.com/MaiReo/housecool-com-scripts/master/src/environments/broker-environment.ts.template \
+  | sed "s/{SERVER_URL}/${SERVER_URL}/g" \
+  | sed "s/{SIGNALR_SERVER_URL}/${SIGNALR_SERVER_URL}/g" \
+  | sed "s/{UPLOAD_SERVER_URL}/${UPLOAD_SERVER_URL}/g" \
+  | tee src/environments/environment.ts
+cp -v src/environments/environment.ts src/environments/environment.aliyun.ts
